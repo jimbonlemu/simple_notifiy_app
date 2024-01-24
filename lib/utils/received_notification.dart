12511b71936f6a06194a075204560b31187faeb2
perompak_kode <iqbaltech29@gmail.com>
2024-01-24 11:12:12 +0700

    update: configuring the android and iOS folders, adding icons and sounds

---
+++ lib/utils/received_notification.dart
@@ -0,0 +1,13 @@
+class ReceivedNotification {
+  final int? id;
+  final String? title;
+  final String? body;
+  final String? payLoad;
+
+  ReceivedNotification({
+    this.id,
+    this.title,
+    this.body,
+    this.payLoad,
+  });
+}
