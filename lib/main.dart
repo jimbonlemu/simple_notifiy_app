bc101a91b6e6a09c2accf6cb7c52531514606d49
perompak_kode <iqbaltech29@gmail.com>
2024-01-25 13:30:50 +0700

    update : separating runApp

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,20 @@ Future<void> main() async {
   final NotificationHelper notificationHelper = NotificationHelper();
   await notificationHelper.initNotification(flutterLocalNotificationsPlugin);
   notificationHelper.requestIOSPermissions(flutterLocalNotificationsPlugin);
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       initialRoute: HomePage.routeName,
       routes: {
         HomePage.routeName: (context) => const HomePage(),
         DetailPage.routeName: (context) => const DetailPage(),
       },
-    ),
-  );
+    );
+  }
 }
-
-
